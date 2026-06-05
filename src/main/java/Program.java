43ce704acbe4f7e64c8d14e37a4f7c56f20f3e4e
Jeremy <jeremybaez919@gmail.com>
2026-06-05 15:08:12 -0700

    exercise 3

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -1,5 +1,6 @@
 import java.sql.*;
 import java.sql.ResultSet;
+import java.util.Scanner;
 
 
 public class Program{
@@ -12,6 +13,16 @@ public class Program{
             );
             System.out.println("Connected!");
 
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("what would you like to do?");
+            System.out.print("1) Display all products");
+            System.out.print("2) Display all customers");
+            System.out.print("Select an option: ");
+
+            int choice = scanner.nextInt();
+
+            if (choice == 1){
+
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery(
                     "SELECT ProductID, ProductName, UnitPrice, UnitsInStock  FROM Products");
@@ -23,8 +34,30 @@ public class Program{
 
                 );
             }
+            } else if (choice == 2){
+                Statement statement = connection.createStatement();
 
-        } catch (SQLException e) {
+                ResultSet resultSet = statement.executeQuery(
+                        "SELECT ContactName, CompanyName, City, Country, Phone "+
+                                "FROM Customers "+
+                                "ORDER BY Country"
+                );
+
+                while (resultSet.next()) {
+
+                    System.out.println(
+                            resultSet.getString("ContactName") +" | " +
+                                    resultSet.getString("CompanyName") + " | " +
+                                    resultSet.getString("City") + " | " +
+                                    resultSet.getString("Country") + " | " +
+                                    resultSet.getString("Phone")
+
+                    );
+                }
+
+                }
+            }
+            catch (SQLException e) {
             System.out.println("Connection failed");
             e.printStackTrace();
         }
